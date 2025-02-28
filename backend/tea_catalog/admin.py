7cc6fbcad3dd671112bc3958ab234c840e590992
AndreiVed <catblues87@gmail.com>
2025-02-28 15:47:18 +0200

    registered to admin panel tea_catalog's models

--- backend/tea_catalog/admin.py
+++ backend/tea_catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from tea_catalog.models import Descriptor, Country, Region, Category, Tea
+
+admin.site.register(Descriptor)
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(Category)
+admin.site.register(Tea)
