350bc0b18a5ab026f13dc9cec639e1aa4e3f9b8d
AndreiVed <catblues87@gmail.com>
2025-04-29 13:09:36 +0300

    add JSONRender to settings and required False to serializer

--- backend/tea_atlas_service/settings.py
+++ backend/tea_atlas_service/settings.py
@@ -172,6 +172,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
 }
 
 SIMPLE_JWT = {
