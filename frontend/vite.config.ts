d0ffb69db2e18131d14ff54c8f881b491f3038e0
mikezhylka <applezhylka@gmail.com>
2025-05-19 21:32:37 +0200

    feat: add aliases for imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
   server: {
     port: 5173,
     // strictPort: true,
-    host: 'localhost',
-  }
-})
+    host: "localhost",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
