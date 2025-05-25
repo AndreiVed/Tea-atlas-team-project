cf713514376ee7a634730e5ce8697756d48edf47
AndreiVed <catblues87@gmail.com>
2025-05-25 19:41:10 +0300

    added config file for docker to frontend dir

--- frontend/src/handlers/refreshToken.ts
+++ frontend/src/handlers/refreshToken.ts
@@ -13,11 +13,7 @@ export const refreshToken = () => {
       return response.json();
     })
     .then((data) => {
-<<<<<<< HEAD
       document.cookie = `refresh=${data.refresh}`;
-=======
-      document.cookie = `refresh=${data.access}`;
->>>>>>> 9097e19ed81b64e0788f0c94f5cf1ce56aecfc09
     })
     .catch((error) => {
       return error;
