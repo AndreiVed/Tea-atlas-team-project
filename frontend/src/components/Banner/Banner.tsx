1a1d393794df7cbdc9e3351aa9869d5734ec519e
mikezhylka <applezhylka@gmail.com>
2025-07-17 10:50:51 +0200

    feat: add loading for banners

--- frontend/src/components/Banner/Banner.tsx
+++ frontend/src/components/Banner/Banner.tsx
@@ -1,5 +1,7 @@
 import { screenEndpoints } from "@/constants/endpoints";
-import { FC } from "react";
+import cn from "classnames";
+import { FC, useState } from "react";
+import { Loader } from "../Loader";
 import styles from "./Banner.module.scss";
 
 type Device = "mobile" | "tablet" | "desktop";
@@ -12,6 +14,8 @@ type Props = {
 export const Banner: FC<Props> = ({ className, baseSrc }) => {
   const { desktop, tablet } = screenEndpoints;
 
+  const [isLoading, setIsLoading] = useState(true);
+
   const insertImageSrc = (device: Device) => {
     const imageFormatIndex = baseSrc.indexOf(".");
     const srcWithoutFormat = baseSrc.slice(0, imageFormatIndex);
@@ -22,7 +26,7 @@ export const Banner: FC<Props> = ({ className, baseSrc }) => {
 
   return (
     <div className={styles[className]}>
-      <picture>
+      <picture className={cn({ [styles["picture--unvisible"]]: isLoading })}>
         <source
           srcSet={insertImageSrc("desktop")}
           media={`(min-width: ${desktop}px`}
@@ -35,10 +39,16 @@ export const Banner: FC<Props> = ({ className, baseSrc }) => {
           className={styles[className]}
           src={insertImageSrc("mobile")}
           alt="Discover world of tea banner"
+          onLoad={() => setIsLoading(false)}
           loading="eager"
           fetchPriority="high"
         />
       </picture>
+      {isLoading && (
+        <div className={styles["loader-wrapper"]}>
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
